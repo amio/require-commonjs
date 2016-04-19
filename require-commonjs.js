17846ea473071e790cac20d8cac245a159613565
Amio <amio.cn@gmail.com>
2016-04-19 12:06:16 +0800

    Use es5.

--- require-commonjs.js
+++ require-commonjs.js
@@ -1,6 +1,6 @@
 (function (scope){
 
-  function requireCommonjs(urls, callback = function(){}){
+  function requireCommonjs(urls, callback){
   	var registry = {}
   	return Promise.all(urls.map(function (url) {
     	return window.fetch(url)
@@ -8,7 +8,7 @@
         .then(function (text) { return evalCommonModule(text) })
     	}
     )).then(function (modules) {
-      callback.apply(this, modules)
+      if (typeof callback === 'function') callback.apply(this, modules)
       return modules
     })
   }
